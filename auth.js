3bffdebec21e9e94abbdea0f6835ccb17fd4159f
nguyenhuynhquocanh81-ux <nguyenhuynhquocanh81@gmail.com>
2026-09-21 15:16:25 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 // updated login logic
 function login(username, password) {
-  if (username === 'admin' && password === '123') {
+  if (username === 'admin' && password === '9999') {
     return true;
   }
   return false;
